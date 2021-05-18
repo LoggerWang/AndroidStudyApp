305a74ddd6346dc26a63ddfb651e206b309cfce9
wanglezhi <wanglezhi@allinmd.cn>
2021-05-18 11:58:26 +0800

    自定义view

--- app/src/main/java/com/legend/androidstudyapp/MainActivity.java
+++ app/src/main/java/com/legend/androidstudyapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.legend.androidstudyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.legend.androidstudyapp.customerview.CustomerViewActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btCustomView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CustomerViewActivity.class));
+            }
+        });
     }
 }
