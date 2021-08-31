7388fd1107f2d1a88989157322c7ae9edacc7fea
wanglezhi <wanglezhi@shareit.com>
2021-08-31 17:51:37 +0800

    testlalalal

--- app/src/test/java/com/legend/androidstudyapp/TestClass.kt
+++
@@ -1,8 +0,0 @@
-package com.legend.androidstudyapp
-
-/**
- * date：2021/8/31 17:47
- * @author wanglezhi
- * desc:
- */class TestClass {
-}
---
+++ app/src/test/java/com/legend/androidstudyapp/Testclassssss.java
@@ -0,0 +1,6 @@
+package com.legend.androidstudyapp;/**
+ * date：2021/8/31 17:50
+ * @author wanglezhi
+ * desc:
+ */public class Testclassssss {
+}
